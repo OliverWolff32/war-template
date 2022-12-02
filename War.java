1fe14ffb8e4432a1955a1d715b33311db31c9812
Oliver Wolff <oliver.wolff@mrjaffesclass.com>
2022-12-02 12:47:04 -0800

    think I am finished with logic. Need to write system print statements

--- War.java
+++ War.java
@@ -8,11 +8,9 @@
 public class War
 {
     // need to write eventloop
-    
+
     //top card is in index0; bottom is the last index
-    
-    
-    
+
     Deck player1Deck;
     Deck player2Deck;
     Deck deck;
@@ -25,53 +23,79 @@ public class War
     {
         // Initializations here...
         deck = new Deck();
-        
+
         deck.initializeNewDeck();
         deck.shuffle();
         Deck[] twoDecks = deck.dealDeck();
         player1Deck = twoDecks[0];
         player2Deck = twoDecks[1];
-        
+
         // ...then run the event loop
         this.runEventLoop();
     }
-    
+
     public void war(Card card1, Card card2) {
         Deck war1 = new Deck();
         Deck war2 = new Deck();
-        
-        while(card1.getRank() == card2.getRank()) {
-            if (player1Deck.getDeckSize() >= 4) {
-                 if(player2Deck.getDeckSize() >= 4) {
-                      // both players have enough cards
-                     
-                      for (int i = 0; i < 3; i++) {
-                           war1.addCardToDeck(player1Deck.dealCardFromDeck());
-                           war2.addCardToDeck(player2Deck.dealCardFromDeck());
-                      }
-                      card1 = player1Deck.dealCardFromDeck();
-                      card2 = player2Deck.dealCardFromDeck();
-                      if (card1.getRank() > card2.getRank()) {
-                          for (int i = 0; i < war1.getDeckSize(); i++) {
-                              player1Deck.addCardToBottomOfDeck(war1.dealCardFromDeck());
-                              player1Deck.addCardToBottomOfDeck(war2.dealCardFromDeck());
-                          }
-                      } else 
-                      if (card1.getRank() < card2.getRank()) {
-                          for (int i = 0; i < war1.getDeckSize(); i++) {
-                              player2Deck.addCardToBottomOfDeck(war1.dealCardFromDeck());
-                              player2Deck.addCardToBottomOfDeck(war2.dealCardFromDeck());
-                          }
-                      } 
-                 }
-                 //player 1 has enough cards, player 2 doesn't
+
+        while(card1.getRank() == card2.getRank() && player1Deck.getDeckSize() > 0 && player2Deck.getDeckSize() > 0) {
+            if (player1Deck.getDeckSize() >= 4 && player2Deck.getDeckSize() >= 4) {
+                // both players have enough
+                war(card1, card2, 3);
                 
-                 
+            } else if (player1Deck.getDeckSize() >= 4) {
+                // player 1 has enough, player 2 doesn't
+                war(card1, card2, player2Deck.getDeckSize());
+                
+            } else if (player2Deck.getDeckSize() >= 4) {
+                // player 2 has enough, player 1 doesn't
+                war(card1, card2, player1Deck.getDeckSize());
+                
+            } else {
+                // neither have enough
+                if(player1Deck.getDeckSize() >= player2Deck.getDeckSize()) {
+                    war(card1, card2, player2Deck.getDeckSize());
+                    //player2 less cards
+                } else {
+                    war(card1, card2, player1Deck.getDeckSize());
+                    //player1 less cards
+                }
             }
-            //neither have enough
+            
         }
     }
-    
+
+    public void war(Card card1, Card card2, int lengthOfWar) {
+        // does 1 war of specified amount of cards
+        
+        Deck war1 = new Deck();
+        Deck war2 = new Deck();
+
+        for (int i = 0; i < lengthOfWar - 1; i++) {
+            war1.addCardToDeck(player1Deck.dealCardFromDeck());
+            war2.addCardToDeck(player2Deck.dealCardFromDeck());
+        }
+        card1 = player1Deck.dealCardFromDeck();
+        card2 = player2Deck.dealCardFromDeck();
+        if (card1.getRank() > card2.getRank()) {
+            for (int i = 0; i < war1.getDeckSize(); i++) {
+                player1Deck.addCardToBottomOfDeck(war1.dealCardFromDeck());
+                player1Deck.addCardToBottomOfDeck(war2.dealCardFromDeck());
+            }
+            player1Deck.addCardToBottomOfDeck(card1);
+            player1Deck.addCardToBottomOfDeck(card2);
+            
+        } else if (card1.getRank() < card2.getRank()) {
+            for (int i = 0; i < war1.getDeckSize(); i++) {
+                player2Deck.addCardToBottomOfDeck(war1.dealCardFromDeck());
+                player2Deck.addCardToBottomOfDeck(war2.dealCardFromDeck());
+            }
+            player2Deck.addCardToBottomOfDeck(card1);
+            player2Deck.addCardToBottomOfDeck(card2);
+            
+        } 
+    }
+
     
     /**
      * This is the game's event loop. The code in here should come
@@ -80,37 +104,40 @@ public class War
     public void runEventLoop() {
         Card card1;
         Card card2;
-        
+
         int turns = 1;
         while (player1Deck.getDeckSize() > 0 && player2Deck.getDeckSize() > 0 && turns <= 300) {
+            System.out.println("Turn: " + turns);
+            
             card1 = player1Deck.dealCardFromDeck(); 
             card2 = player2Deck.dealCardFromDeck(); 
-            
+
             if (card1.getRank() < card2.getRank()) {
                 player2Deck.addCardToBottomOfDeck(card1);
                 // player 2 wins war
             } else if (card1.getRank() == card2.getRank()) {
                 // war occurs 
                 war(card1, card2);
-                
             } else if (card1.getRank() > card2.getRank()) {
                 player1Deck.addCardToBottomOfDeck(card2);
                 //player 1 wins war
-            }            
+            }
+            
         }
-        
-        
+        if (player1Deck.getDeckSize() > 0) {
+            System.out.println("----------Player 1 wins!----------");
+        } else {
+            System.out.println("----------Player 2 wins!----------");
+        }
+
     }
-    
-    
-    
-    
+
     /**
      * The main method is called when Java starts your program
      */
     public static void main(String[] args) {
         War war = new War();
-        
+
     }
 
 }
